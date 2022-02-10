809ca129f29deaeddb77f827599b1f6c7e8b7936
lolilofit <usovadasha@myttk.ru>
2022-02-10 13:05:37 +0300

    Fix build

--- frameworks/Kotlin/ktor/ktor-snapshot/src/main/kotlin/org/jetbrains/ktor/benchmarks/Hello.kt
+++ frameworks/Kotlin/ktor/ktor-snapshot/src/main/kotlin/org/jetbrains/ktor/benchmarks/Hello.kt
@@ -1,13 +1,13 @@
 package org.jetbrains.ktor.benchmarks
 
 import com.zaxxer.hikari.*
-import io.ktor.application.*
-import io.ktor.features.*
-import io.ktor.html.*
 import io.ktor.http.*
 import io.ktor.http.content.*
-import io.ktor.response.*
-import io.ktor.routing.*
+import io.ktor.server.application.*
+import io.ktor.server.html.*
+import io.ktor.server.plugins.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import kotlinx.coroutines.*
 import kotlinx.html.*
 import kotlinx.serialization.*
